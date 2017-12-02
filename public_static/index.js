70d909b6ab56082187ebfe2d7154b14831aca5cd
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-12-02 13:51:48 +0530

    socket chat start
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- public_static/index.js
+++ public_static/index.js
@@ -2,3 +2,13 @@
  * Created by rishabhkhanna on 02/12/17.
  */
 var socket = io();
+
+$(function () {
+   var msg = $('#msg');
+   var sendBtn = $('#send');
+   var chat = $('#chat');
+
+   sendBtn.click(function () {
+
+   })
+});
