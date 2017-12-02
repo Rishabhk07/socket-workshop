273adee7ecc88bf239d77dbd6872fdc99909f786
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-12-02 12:59:01 +0530

    express static
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
  */
 const express = require('express');
 const app = express();
+const path = require('path');
+
+
 
 app.get('/', function (req,res) {
     console.log(req.query);
@@ -13,6 +16,8 @@ app.get('/', function (req,res) {
 app.get('/rishabh',function (req,res) {
     res.send("Khanna");
 })
+
+app.use('/',express.static(path.join(__dirname, '/public_static')));
 
 app.listen(9090,function () {
     console.log("server has started");
