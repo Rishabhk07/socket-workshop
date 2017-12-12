b125b452caef96dfef080f7aae28f0d082984918
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-12-12 13:29:40 +0530

    updated
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app.js
+++ app.js
@@ -12,19 +12,42 @@ const socketio = require('socket.io');
 //pass the server on which you have to create a socket
 const io = socketio(server);
 const path = require('path');
+let users = {};
 
 io.on('connection', function (socket) {
+
+
     console.log("a new connection has connected");
+
     socket.on('new_msg', function (data) {
-        console.log(data);
-        io.emit('chat', data);
+        if (data.msg.charAt(0) == '@'){
+            console.log(data);
+            console.log(data.msg.split(" ")[0]);
+            var msg = data.msg.substring(data.msg.indexOf(" ") + 1)
+            console.log(msg)
+            io.to(users[data.msg.split(" ")[0]]).emit('chat',{
+                username: data.username,
+                msg : msg
+            });
+
+        }else{
+            console.log(data);
+            io.emit('chat', data);
+        }
+
     })
+
+    socket.on('store_user', function (data) {
+        users["@" + data.user] = socket.id
+        console.log(users);
+    })
+
+
 })
 
 app.use('/', express.static(path.join(__dirname, "/public_static")));
 
 
-
-server.listen(8989,function () {
+server.listen(8989, function () {
     console.log("Magic happens at 8989");
 })
--- public_static/index.js
+++ public_static/index.js
@@ -2,17 +2,26 @@
  * Created by rishabhkhanna on 02/12/17.
  */
 var socket = io();
-
+var user = prompt("who you ?")
 $(function () {
-   var msg = $('#msg');
-   var sendBtn = $('#send');
-   var chat = $('#chat');
+    var msg = $('#msg');
+    var sendBtn = $('#send');
+    var chat = $('#chat');
 
-   sendBtn.click(function () {
-        socket.emit('new_msg',msg.val());
-   })
+
+    sendBtn.click(function () {
+        socket.emit('new_msg', {
+            username: user,
+            msg: msg.val()
+        });
+    })
+
 
     socket.on('chat', function (data) {
-        chat.append("<li>"+ data+"</li>")
+        chat.append("<li>" + data.username + ": " + data.msg + "</li>")
+    })
+
+    socket.emit('store_user', {
+        user: user
     })
 });
